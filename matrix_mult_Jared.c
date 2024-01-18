30333cea61aff5b2a54ddbc10f886f8be74e4814
JReiling1 <reiling1@msu.edu>
2024-01-18 10:08:34 -0500

    Changed the matrices to floats and freed matrix memory after execution

--- matrix_mult_Jared.c
+++ matrix_mult_Jared.c
@@ -5,24 +5,27 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Sources:
 // ChatGPT assisted in the syntax of the program below
+// https://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c
 
 // Create a random nxn matrix
 // TODO: Why does initial random values of matrix all the same for each time method is called
-int** generate_matrix(int size) {
-    int** matrix = (int**)malloc(size * sizeof(int*));
+float** generate_matrix(int size) {
+    float** matrix = (float**)malloc(size * sizeof(int*));
     
     for (int i = 0; i < size; i++) {
-        matrix[i] = (int*)malloc(size * sizeof(int));
+        matrix[i] = (float*)malloc(size * sizeof(int));
     }
 
-    // Seed is need to generate a random number
-    srand(time(NULL));
+    // Random seed for number generation
+    srand(time(0));
 
     // Fill the matrix with random numbers
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            matrix[i][j] = rand() % 100; // Adjust the range
+            // matrix[i][j] = rand() % 100; // Adjust the range
+            matrix[i][j] =  (float)rand()/(float)(RAND_MAX);
         }
     }
 
@@ -32,7 +35,7 @@ int** generate_matrix(int size) {
 
 
 // The function below is made to print the elements of the nxn matrix
-void print_matrix(int** matrix, int size) {
+void print_matrix(float** matrix, int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             printf("%d\t", matrix[i][j]);
@@ -41,11 +44,12 @@ void print_matrix(int** matrix, int size) {
     }
 }
 
-static int** multiply_matrices(int** matrix_a, int** matrix_b, int** matrix_c, int n) {
+// Function that multiplies matrix_a and matrix_b. Then adds the product to elements in matrix_c
+static float** multiply_matrices(float** matrix_a, float** matrix_b, float** matrix_c, int size) {
     // From first example in HPSC textbook page 49
-    for (int idx = 0; idx < n; idx++){
-        for (int idy = 0; idy < n; idy++){
-            for (int idz = 0; idz < n; idz++){
+    for (int idx = 0; idx < size; idx++){
+        for (int idy = 0; idy < size; idy++){
+            for (int idz = 0; idz < size; idz++){
                 // TODO: Make sure c is updated accordingly.
                 matrix_c[idx][idy] += matrix_a[idx][idz] * matrix_b[idz][idy];
             }
@@ -55,25 +59,44 @@ static int** multiply_matrices(int** matrix_a, int** matrix_b, int** matrix_c, i
 }    
 
 
+// Function that frees memory for matrix_a, matrix_b, and matrix_c
+void free_matrix_memory(float** matrix_a, float**matrix_b, float** matrix_c, int size) {
+    for (int i = 0; i < size; i++) {
+        free(matrix_a[i]);
+        free(matrix_b[i]);
+        free(matrix_c[i]);
+    }
+    free(matrix_a);
+    free(matrix_b);
+    free(matrix_c);
+}
+
 int main() {
     int size = 2; // size of the nxn matrix
 
     // Create randomly generated matrices
-    int** matrix_a = generate_matrix(size);
-    int** matrix_b = generate_matrix(size);
-    int** matrix_c = generate_matrix(size);
+    float** matrix_a = generate_matrix(size);
+    float** matrix_b = generate_matrix(size);
+    float** matrix_c = generate_matrix(size);
 
     // Print values of randomly generated matrices
     printf("Initial values of matrix a, b, and c \n");
     print_matrix(matrix_a, size);
+    printf("\n");
     print_matrix(matrix_b, size);
+    printf("\n");
     print_matrix(matrix_c, size);
+    printf("\n");
 
     // Compute multiplication of matrices
-    int** result = multiply_matrices(matrix_a, matrix_b, matrix_c, size);
+    float** result = multiply_matrices(matrix_a, matrix_b, matrix_c, size);
 
     // Print result matrix
+    printf("Result of Matrix Multiplication \n");
     print_matrix(result, size);
+
+    // Free the memory for matrices
+    free_matrix_memory(matrix_a, matrix_b, matrix_c, size);
 
     return 0;
 }
