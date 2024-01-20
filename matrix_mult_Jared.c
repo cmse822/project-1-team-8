9877a5b73c141e978ca73f2fb7f3cf2ac50a4184
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2024-01-20 14:09:59 -0500

    Added trials and sizes array

--- matrix_mult_Jared.c
+++ matrix_mult_Jared.c
@@ -80,73 +80,83 @@ int main() {
     srand(time(NULL));
 
     // Generate a random number between 1 and 500
-    int size = 1 + rand() % (500); // size of the nxn matrix
+    int sizes[] = {5, 25, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000}; // size of the nxn matrix
+    int sizesArrayLength = sizeof(sizes) / sizeof(sizes[0]);
 
-    // Print the random number
-    printf("Random Number: %d\n", size);
+    // Number of trials per matrix size
+    int num_trials = 3;
+    int size;
 
-    // Create randomly generated matrices
-    float** matrix_a = generate_matrix(size);
-    float** matrix_b = generate_matrix(size);
-    float** matrix_c = generate_matrix(size);
+    for (int i = 0; i < sizesArrayLength; i++) {
+        size = sizes[i];
+        
+        printf("Size of the matrix: %d \n", size);
 
-    // Print values of randomly generated matrices
-    // printf("Initial values of matrix a, b, and c \n");
-    // print_matrix(matrix_a, size);
-    // printf("\n");
-    // print_matrix(matrix_b, size);
-    // printf("\n");
-    // print_matrix(matrix_c, size);
-    // printf("\n");
+        for (int trial = 1; trial <= num_trials; trial ++) {
+            // Create randomly generated matrices
+            float** matrix_a = generate_matrix(size);
+            float** matrix_b = generate_matrix(size);
+            float** matrix_c = generate_matrix(size);
 
-    // Clock time to measure multipy_matrices time
-    struct timeval startTime, endTime;
+            // Print values of randomly generated matrices
+            // printf("Initial values of matrix a, b, and c \n");
+            // print_matrix(matrix_a, size);
+            // printf("\n");
+            // print_matrix(matrix_b, size);
+            // printf("\n");
+            // print_matrix(matrix_c, size);
+            // printf("\n");
 
-    // Compute multiplication of matrices and measure time
-    gettimeofday(&startTime, NULL);
-    float** result = multiply_matrices(matrix_a, matrix_b, matrix_c, size);
-    gettimeofday(&endTime, NULL);
+            // Clock time to measure multipy_matrices time
+            struct timeval startTime, endTime;
 
-    // Calculate the amount of time used
-    double executionTime = (double)(endTime.tv_sec - startTime.tv_sec) +
-                           (double)(endTime.tv_usec - startTime.tv_usec) / 1000000.0;
+            // Compute multiplication of matrices and measure time
+            gettimeofday(&startTime, NULL);
+            float** result = multiply_matrices(matrix_a, matrix_b, matrix_c, size);
+            gettimeofday(&endTime, NULL);
+
+            // Calculate the amount of time used
+            double executionTime = (double)(endTime.tv_sec - startTime.tv_sec) +
+                                (double)(endTime.tv_usec - startTime.tv_usec) / 1000000.0;
 
 
-    // Calculate the number of mflops/s for matrix multiplication
-    double Mflops_total = (2*pow(size,3) - pow(size,2)) / pow(10,6);
-    double performance_mflops = ((double) Mflops_total) / ((double) executionTime);
-    printf("Total number of Mflops: %f \n", Mflops_total);
-    printf("Excution time: %f in seconds \n", executionTime);
-    printf("Total performance flops in Mflops/s: %f \n", performance_mflops);
+            // Calculate the number of mflops/s for matrix multiplication
+            double Mflops_total = (2*pow(size,3) - pow(size,2)) / pow(10,6);
+            double performance_mflops = ((double) Mflops_total) / ((double) executionTime);
+            printf("Total number of Mflops: %f \n", Mflops_total);
+            printf("Excution time: %f in seconds \n", executionTime);
+            printf("Total performance flops in Mflops/s: %f \n", performance_mflops);
 
-    // Print result matrix
-    // printf("Result of Matrix Multiplication \n");
-    // print_matrix(result, size);
+            // Print result matrix
+            // printf("Result of Matrix Multiplication \n");
+            // print_matrix(result, size);
 
-    printf("Wall time take for matrix multiplication: %f seconds for N = %d. \n", executionTime, size);
-    printf("Performance for matrix multiplication in Mflop/s: %f for N = %d. \n", performance_mflops, size);
+            printf("Wall time take for matrix multiplication: %f seconds for N = %d. \n", executionTime, size);
+            printf("Performance for matrix multiplication in Mflop/s: %f for N = %d. \n", performance_mflops, size);
 
-    // Open the CSV file for writing
-    FILE *csvFile = fopen("matrix_multiplication_team_8.csv", "a");
+            // Open the CSV file for writing
+            FILE *csvFile = fopen("matrix_multiplication_team_8.csv", "a");
 
-    if (csvFile == NULL) {
-        fprintf(stderr, "Error opening file.\n");
-        return 1;
+            if (csvFile == NULL) {
+                fprintf(stderr, "Error opening file.\n");
+                return 1;
+            }
+
+            // Write header to the CSV file
+            fprintf(csvFile, "Trial,N,Time,Flops,Performance\n");
+
+            // Write data to the CSV file
+            fprintf(csvFile, "%d,%d,%f,%f,%f\n", trial, size, executionTime, Mflops_total, performance_mflops);
+
+            // Close the CSV file
+            fclose(csvFile);
+
+            printf("CSV file written successfully.\n");
+
+            // Free the memory for matrices
+            free_matrix_memory(matrix_a, matrix_b, matrix_c, size);
+        }
     }
-
-    // Write header to the CSV file
-    fprintf(csvFile, "N,Time,Flops,Performance\n");
-
-    // Write data to the CSV file
-    fprintf(csvFile, "%d,%f,%f,%f\n", size, executionTime, Mflops_total, performance_mflops);
-
-    // Close the CSV file
-    fclose(csvFile);
-
-    printf("CSV file written successfully.\n");
-
-    // Free the memory for matrices
-    free_matrix_memory(matrix_a, matrix_b, matrix_c, size);
-
+    
     return 0;
 }
